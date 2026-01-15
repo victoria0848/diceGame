fc9647972e6190f5ac95abf86ba4a5648fea6422
victoria0848 <viku5@elev.techcollege.dk>
2026-01-15 11:05:27 +0100

    tilføjet variables og struktur

---
+++ dicegame/src/App.tsx
@@ -0,0 +1,11 @@
+import { DiceGame } from "./components/diceGame";
+
+function App() {
+  return (
+    <main>
+      <DiceGame />
+    </main>
+  );
+}
+
+export default App;
---
+++ dicegame/src/components/diceGame.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Dice } from "./dice";
+import { Controls } from "./controls";
+import styles from "./DiceGame.module.scss";
+
+export const DiceGame = () => {
+  const [dice, setDice] = useState<number>(rollDice());
+  const [result, setResult] = useState<string>("");
+
+  function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  function handleGuess(type: "higher" | "lower") {
+    const newDice = rollDice();
+
+    if (
+      (type === "higher" && newDice > dice) ||
+      (type === "lower" && newDice < dice)
+    ) {
+      setResult("✅ Rigtigt!");
+    } else {
+      setResult("❌ Forkert!");
+    }
+
+    setDice(newDice);
+  }
+
+  return (
+    <section className={styles.game}>
+      <h1>🎲 Højere eller Lavere</h1>
+
+      <Dice value={dice} />
+
+      <Controls onGuess={handleGuess} />
+
+      {result && <p className={styles.result}>{result}</p>}
+    </section>
+  );
+};
